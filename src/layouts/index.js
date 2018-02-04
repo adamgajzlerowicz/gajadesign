f93c29071dfa24612640e40ca789d7f56a5743e3
adam.gajzlerowicz <nelf86@gmail.com>
2018-02-04 13:59:04 +0100

    push

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
@@ -14,7 +13,6 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'poznań, reklama, agencja reklamy, gajzlerowicz, etykiety' },
       ]}
     />
-    <Header />
     <div
       style={{
         margin: '0 auto',
